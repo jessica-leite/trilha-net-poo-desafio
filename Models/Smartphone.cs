f4736619291a74e0a2f4ed09485a2d4af74a0a2d
Jess <jessica.sileite@gmail.com>
2025-07-09 15:40:52 -0300

    Propriedades populadas por construtor

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -7,10 +7,12 @@ namespace DesafioPOO.Models
         protected string IMEI { get; set; }
         protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
