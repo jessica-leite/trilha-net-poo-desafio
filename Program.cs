9b2435daea019a58f17e17023469ad9d580f89ea
Jess <jessica.sileite@gmail.com>
2025-07-09 15:48:47 -0300

    Implementados testes na classe Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("11999999999", "Nokia 3310", "1234567890", 64);
+Iphone iphone = new Iphone("11988888888", "Iphone 13", "0987654321", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Angry Birds");
